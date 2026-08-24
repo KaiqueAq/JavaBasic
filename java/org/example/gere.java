0228ecb4ced0edfdfcfe8449347ffe471a76b2cf
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-08-24 19:15:49 -0300

    Revert "Add files via upload"
    This reverts commit 1eee347f8c0363a4d7c8bb638971ba7eea8c2ea4.

--- java/org/example/gere.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-public class gere {
-    static void main() {
-        for (int i=100; i <= 120; i=i+2){
-            System.out.printf("Valor de i: %d%n" , i);
-        }
-    }
-}
